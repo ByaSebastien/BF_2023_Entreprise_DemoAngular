87aabe81c763a5f5e051d4ee429fc0b71705c562
Sebastien <byasebastien@hotmail.com>
2023-11-22 10:15:37 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'BF_2023_Entreprise_DemoAngular';
+}
